502818f10861759fa37776092f6950284d8456cb
mwxka <jeremy.mutiso@strathmore.edu>
2024-09-18 08:01:08 +0300

    function and structs added, iterative structures used
    i created functions and used while loops for game matchmaking,  i removed comments
    
    Co-Authored-By: nicolewanjiru <179138998+nicolewanjiru@users.noreply.github.com>
    Co-Authored-By: Trisha  <178927837+Wakabs@users.noreply.github.com>
    Co-Authored-By: charlotteharmit <179139505+charlotteharmit@users.noreply.github.com>

--- project.cpp
+++ project.cpp
@@ -6,38 +6,32 @@
 
 using namespace std;
 
-// Structure to hold team information
 struct Team {
-    string name;   // Team's name
-    string town;   // The town the team is based in
-    string stadium; // The stadium where the team plays home games
+    string name;
+    string town;
+    string stadium;
 };
 
-// Structure to hold match information
 struct Match {
-    string home_team;  // Name of the team hosting the match
-    string away_team;  // Name of the team visiting for the match
-    string stadium;    // Stadium where the match is taking place
-    string town;       // Town where the stadium is located
-    int leg;           // 1 for the first leg, 2 for the second leg
-    int weekend;       // Which weekend this match will take place on
+    string home_team;
+    string away_team;
+    string stadium;
+    string town;
+    int leg;
+    int weekend;
 };
 
-// Function to load teams from CSV file
 vector<Team> loadTeamsFromCSV(const string& filename) {
     vector<Team> teams;
     ifstream file(filename);
     string line;
 
-    // Skip the first line (headers)
     getline(file, line);
 
-    // Read each line from the CSV file
     while (getline(file, line)) {
         stringstream ss(line);
         string name, town, stadium;
 
-        // Split by tabs
         getline(ss, name, '\t');
         getline(ss, town, '\t');
         getline(ss, stadium, '\t');
@@ -47,24 +41,34 @@ vector<Team> loadTeamsFromCSV(const string& filename) {
     return teams;
 }
 
-// Function to generate home and away matches
 vector<Match> generateFixtures(const vector<Team>& teams) {
     vector<Match> fixtures;
     int weekend = 1;
 
     for (size_t i = 0; i < teams.size(); ++i) {
         for (size_t j = i + 1; j < teams.size(); ++j) {
-            // Home and away matches
             Match home_match = {teams[i].name, teams[j].name, teams[i].stadium, teams[i].town, 1, weekend};
             Match away_match = {teams[j].name, teams[i].name, teams[j].stadium, teams[j].town, 2, weekend};
 
             fixtures.push_back(home_match);
             fixtures.push_back(away_match);
 
-            if (fixtures.size() % 2 == 0) weekend++; // Move to next weekend every two matches
+            if (fixtures.size() % 2 == 0) weekend++;
         }
     }
     return fixtures;
 }
 
-/
+int main() {
+    vector<Team> teams = loadTeamsFromCSV("teams.csv");
+
+    vector<Match> fixtures = generateFixtures(teams);
+
+    for (const auto& match : fixtures) {
+        cout << "Weekend: " << match.weekend << " | "
+             << match.home_team << " vs " << match.away_team << " at "
+             << match.stadium << ", " << match.town << " (Leg " << match.leg << ")" << endl;
+    }
+
+    return 0;
+}
